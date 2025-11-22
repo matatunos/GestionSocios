08225ff4e6c55e5f41de95f46a9467e17a5c1828
matatunos <matatunos@gmail.com>
2025-11-22 10:31:21 +0100

    Fix case sensitivity for config.php include in fix script

--- public/fix_paths.php
+++ public/fix_paths.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Include database connection
-require_once __DIR__ . '/../src/Config/Config.php';
+require_once __DIR__ . '/../src/Config/config.php';
 require_once __DIR__ . '/../src/Config/Database.php';
 
 try {
