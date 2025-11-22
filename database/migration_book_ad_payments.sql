1b1e3ac95038f9ac2e10facef9753f7f0a42ae48
matatunos <matatunos@gmail.com>
2025-11-22 19:39:15 +0100

    Fix: Añadir 'book_ad' al ENUM payment_type en migración

--- database/migration_book_ad_payments.sql
+++ database/migration_book_ad_payments.sql
@@ -1,5 +1,11 @@
 -- Add book_ad_id column to payments table for tracking book ads payments
+-- Also add 'book_ad' to payment_type ENUM
 
+-- First, add the new payment_type value
+ALTER TABLE payments 
+MODIFY COLUMN payment_type ENUM('fee', 'event', 'donation', 'book_ad') DEFAULT 'fee';
+
+-- Add book_ad_id column
 ALTER TABLE payments 
 ADD COLUMN IF NOT EXISTS book_ad_id INT DEFAULT NULL,
 ADD CONSTRAINT fk_payments_book_ad 
