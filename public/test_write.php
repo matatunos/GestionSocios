d4f9a9f8abae68a34f418846fe68270a97a6d24a
matatunos <matatunos@gmail.com>
2025-11-27 20:21:23 +0100

    Chore: Remove unused debug and temporary files

--- public/test_write.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = __DIR__ . DIRECTORY_SEPARATOR . 'test.txt';
-$result = @file_put_contents($file, "prueba de escritura\n");
-if ($result === false) {
-    echo "No se pudo escribir en $file";
-} else {
-    echo "Archivo creado correctamente: $file";
-}
-?>
